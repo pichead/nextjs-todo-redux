33a311e0717ddbfd0c907db084f75a24250de5a2
pichead <61868523+pichead@users.noreply.github.com>
2024-11-25 03:43:34 +0700

    fix(middleware): fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,21 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    const res = NextResponse.next()
-    res.headers.append('ACCESS-CONTROL-ALLOW-ORIGIN', '*')
-    return res
+  console.log(req.nextUrl);
+
+  const res = NextResponse.next();
+
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // เปลี่ยน "*" เป็นโดเมนที่ต้องการอนุญาตหากไม่ต้องการอนุญาตทุกที่
+  res.headers.append("Access-Control-Allow-Methods", "GET, DELETE, PATCH, POST, PUT");
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
 }
 
 export const config = {
-    matcher: ['/api/:path*']
-}
+  matcher: ["/api/:path*"], // ใช้ matcher เพื่อให้ middleware ทำงานเฉพาะ endpoint ที่ขึ้นต้นด้วย "/api/"
+};
